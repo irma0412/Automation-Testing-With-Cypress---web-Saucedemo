55c40da685c63f6ba0a3b0f717274a34c1240cf2
irma suryani <irmairma1204@gmail.com>
2025-07-10 18:01:25 +0700

    update file login.cy.js & loginPages.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+import loginPages from "../support/pages/loginPages";
+
 describe ("Testing Web SauceDemo", () => {
     beforeEach(() =>{
         cy.visit('https://www.saucedemo.com/')
@@ -8,49 +10,39 @@ describe ("Testing Web SauceDemo", () => {
     })
 
     it ("✅Succes login", () => {
-        cy.get('#user-name').type('standard_user');
-        cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
-             
-        // Validation to ensure the URL points to the inventory page
-        cy.url().should('include', '/inventory.html');
-        // Validation to ensure there are elements on the page after login
-        cy.get('.inventory_list').should('be.visible');
-        // Validation to ensure the shopping cart appears
-        cy.get('.shopping_cart_link').should('be.visible');
-        cy.log('✅succes login');
-        
+        loginPages.fillUsername('standard_user');
+        loginPages.fillPassword('secret_sauce');
+        loginPages.clickLogin();           
+        // Validation
+        loginPages.verifyLoginSucces()           
         
     });
 
     it("❌Failed login because username invalid", ()=>{
-        cy.get('#user-name').type('standard');
-        cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
+        loginPages.fillUsername('standard');
+        loginPages.fillPassword('secret_sauce');
+        loginPages.clickLogin();
         // validation to ensure there is an error message
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
-        cy.log('❌Failed login because username invalid');
+        loginPages.verifyErrorMessage('Epic sadface: Username and password do not match any user in this service');
    
     });
 
     it("❌Failed login because password invalid", ()=>{
-        cy.get('#user-name').type('standard_user');
-        cy.get('#password').type('secret');
-        cy.get('#login-button').click();
+        loginPages.fillUsername('standard_user');
+        loginPages.fillPassword('secret');
+        loginPages.clickLogin();
         // validation to ensure there is an error message
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
-        cy.log('❌Failed login because password invalid');
+        loginPages.verifyErrorMessage('Epic sadface: Username and password do not match any user in this service');
         
      
     });
 
     it("❌Failed login because username&password invalid", ()=>{
-        cy.get('#user-name').type('standard');
-        cy.get('#password').type('secret');
-        cy.get('#login-button').click();
+        loginPages.fillUsername('standard');
+        loginPages.fillPassword('secret');
+        loginPages.clickLogin();
         // validation to ensure there is an error message
-        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
-        cy.log('❌Failed login because username&password invalid');
+        loginPages.verifyErrorMessage('Epic sadface: Username and password do not match any user in this service');
    
     });
 
--- cypress/support/pages/loginPages.js
+++ cypress/support/pages/loginPages.js
@@ -15,12 +15,16 @@ class LoginPage {
     }
 
     verifyLoginSucces(){
+        //  Validation to ensure the URL points to the inventory page
         cy.url().should('include', '/inventory.html');
+        // Validation to ensure there are elements on the page after login
         cy.get('.inventory_list').should('be.visible');
+        // Validation to ensure the shopping cart appears
         cy.get('.shopping_cart_link').should('be.visible');
     }
 
     verifyErrorMessage(text){
+        // Validation to ensure there is an error message
         cy.get('[data-test="error"]').should('contain', text);
 
     }
