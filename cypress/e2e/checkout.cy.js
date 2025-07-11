2c71bcc9bd75c01d8c82a4b2700748a13eb53385
irma suryani <irmairma1204@gmail.com>
2025-07-11 08:45:48 +0700

    update file checkout.cy.js

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,9 +1,12 @@
+import checkoutPage from "../support/pages/checkoutPage";
+import loginPages from "../support/pages/loginPages";
+
 describe("Checkout Produk Web SauceDemo", ()=>{
     beforeEach(()=>{
-        cy.visit("https://www.saucedemo.com");
-        cy.xpath('//*[@id="user-name"]').type('standard_user');
-        cy.xpath('//*[@id="password"]').type('secret_sauce');
-        cy.xpath('//*[@id="login-button"]').click();
+        loginPages.fillUsername('standar_user');
+        loginPages.fillPassword('secret_sauce');
+        loginPages.clickLogin();
+        loginPages.verifyLoginSucces();
 
     });
     afterEach(()=>{
@@ -11,83 +14,66 @@ describe("Checkout Produk Web SauceDemo", ()=>{
     });
 
     it("✅Checkout 2 product", ()=>{
-        // add produk to card
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-fleece-jacket"]').click();
-
-        // click button to checkout 
-        cy.xpath('//*[@id="shopping_cart_container"]/a').click();
-        cy.xpath('//*[@id="checkout"]').click();
-
-        // input name and post code
-        cy.xpath('//*[@id="first-name"]').type('Irma');
-        cy.xpath('//*[@id="last-name"]').type('Suryani');
-        cy.xpath('//*[@id="postal-code"]').type('11223');
-        
-        // click button to payment
-        cy.xpath('//*[@id="continue"]').click();
-        cy.xpath('//*[@id="finish"]').click();
-        cy.log('✅succes payment');
+        // add produk to cart
+        checkoutPage.addToCart(['sauce-labs-bolt-t-shirt', 'sauce-labs-fleece-jacket']);
+        checkoutPage.openCart();
+        // validation
+        checkoutPage.verifyOnCartPage();
+        checkoutPage.clickCheckout();
+        checkoutPage.fillCustomerInfo('irma', 'suryani', '11223');
+        checkoutPage.clickContinue();
+        checkoutPage.clickFinish();
+        // validation
+        checkoutPage.verifySuccesCheckout();         
+        cy.log('✅succes to checkout 2 product');
     });
     
     it("✅Checkout 3 product", ()=>{
-        // add produk to card
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-bike-light"]').click();
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-fleece-jacket"]').click();
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-onesie"]').click();
-
-        // click button to checkout 
-        cy.xpath('//*[@id="shopping_cart_container"]/a').click();
-        cy.xpath('//*[@id="checkout"]').click();
-
-        // input name and post code
-        cy.xpath('//*[@id="first-name"]').type('Irma');
-        cy.xpath('//*[@id="last-name"]').type('Suryani');
-        cy.xpath('//*[@id="postal-code"]').type('11223');
-        
-        // click button to payment
-        cy.xpath('//*[@id="continue"]').click();
-        cy.xpath('//*[@id="finish"]').click();
-        cy.log('✅succes payment');
+        // add produk to cart
+        checkoutPage.addToCart(['sauce-labs-bike-light', 'sauce-labs-fleece-jacket', 'sauce-labs-onesie']);
+        checkoutPage.openCart();
+        // validation
+        checkoutPage.verifyOnCartPage();
+        checkoutPage.clickCheckout();
+        checkoutPage.fillCustomerInfo('irma', 'suryani', '11223');
+        checkoutPage.clickContinue();
+        checkoutPage.clickFinish();
+        // validation
+        checkoutPage.verifySuccesCheckout();
+        cy.log('✅succes to checkout 3 product');
     });
     it("❌Checkout without product", ()=>{
-    // ✅ Expected: User should NOT be able to proceed checkout without any product
-    // 🐞 Actual: User still can finish checkout → this is a BUG in the system
-
+  
         // click button to checkout 
-        cy.xpath('//*[@id="shopping_cart_container"]/a').click();
-        cy.xpath('//*[@id="checkout"]').click();
-
-
-        // input name and post code
-        cy.xpath('//*[@id="first-name"]').type('Irma');
-        cy.xpath('//*[@id="last-name"]').type('Suryani');
-        cy.xpath('//*[@id="postal-code"]').type('11223');
-        
-        // click button to payment
-        cy.xpath('//*[@id="continue"]').click();
-        cy.xpath('//*[@id="finish"]').click();
-       
+        checkoutPage.openCart();
+        checkoutPage.clickCheckout();
+        // validation
+        checkoutPage.verifyOnCartPage();
+        checkoutPage.clickCheckout();
+        checkoutPage.fillCustomerInfo('irma', 'suryani', '11223');
+        checkoutPage.clickContinue();
+        checkoutPage.clickFinish(); 
+        // validation       
+        // expectation failed, because this is a BUG from SauceDemo — it shouldn't be possible to finish without items
+        cy.url().should('include', 'checkout-complete.html');
+        cy.contains('THANK YOU FOR YOUR ORDER').should('be.visible');
+        cy.log('BUG: Checkout successful even though the cart is empty');
+ 
     });
   
-    it("✅cancel checkout", ()=>{
-        // add produk to card
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-fleece-jacket"]').click();
-        cy.xpath('//*[@id="add-to-cart-sauce-labs-onesie"]').click();
-
-        // click button to checkout 
-        cy.xpath('//*[@id="shopping_cart_container"]/a').click();
-        cy.xpath('//*[@id="checkout"]').click();
-
-        // input name and post code
-        cy.xpath('//*[@id="first-name"]').type('Irma');
-        cy.xpath('//*[@id="last-name"]').type('Suryani');
-        cy.xpath('//*[@id="postal-code"]').type('11223');
-        
-        // click button cancel
-        cy.xpath('//*[@id="cancel"]').click();
-        cy.xpath('//*[@id="continue-shopping"]').click();
-        cy.log('✅succes cancel payment');
+    it("✅cancel checkout", ()=>{     
+        // add produk to cart
+        checkoutPage.addToCart(['sauce-labs-bolt-t-shirt', 'sauce-labs-fleece-jacket']);
+        checkoutPage.openCart();
+        // validation
+        checkoutPage.verifyOnCartPage();
+        checkoutPage.clickCheckout();
+        checkoutPage.fillCustomerInfo('irma', 'suryani', '11223');
+        checkoutPage.cancelCheckout()
+        checkoutPage.backToShopping();
+        // validation
+        cy.ur().sould('include', 'inventory.html');
+        cy.log('✅Checkout is canceled and returned to the product page');
 
     });
 });
